756f08fece4a6d245377de803bf53210ab58f56c
PratikChavare02 <pratikchavare69@gmail.com>
2025-02-20 03:08:03 +0530

    Removed all files

--- src/social.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function social({Logos,Content}) {
-  return (
-    <div className='text-white'>
-        <img className='h-10 m-3' src={Logos} alt={Content}/>
-    </div>
-  )
-}
